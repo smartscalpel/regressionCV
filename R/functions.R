de9f0881afc0940a8358f912d23fef299254201e
Anatoly Anatoly <lptolik@gmail.com>
2020-04-20 20:24:11 +0100

    some statistic is collected

--- R/functions.R
+++ R/functions.R
@@ -11,7 +11,8 @@ library(caret)
 library(randomForest)
 library(doParallel)
 
-path<-'~/Downloads/peak2019.full/'
+#path<-'~/Downloads/peak2019.full/'
+path<-'/Users/lptolik/Dropbox/Скальпель/DBData/regression/RegReports/'
 
 get_model_fname<-function(ms_setup,diag,expt,method,idx){
   res=ms_setup[1]
@@ -27,7 +28,7 @@ get_fm_fname<-function(ms_setup,ddiag,dexpt){
   res=ms_setup[1]
   mode=ms_setup[2]
   mz=ms_setup[3]
-  dev=ifelse(expt==1,2,4)
+  dev=ifelse(dexpt==1,2,4)
   fname<-sprintf('peak2019.diag_%d.expt_%d.res_%d.mode_%d.dev_%d.mz_%d.fm.rds',
                  ddiag,dexpt,res,mode,dev,mz)
   return(fname)
@@ -46,7 +47,7 @@ get_model<-function(ms_setup,diag,expt,method,idx){
 
 mapMZ<-function(model,fm){
   modMZ<-as.numeric(sub('^MZ_','',model$xNames))
-  dataMZ<-as.numeric(sub('^MZ_','',names(fm)[grep('^MZ',names(fm))])))
+  dataMZ<-as.numeric(sub('^MZ_','',names(fm)[grep('^MZ',names(fm))]))
 }
 
 predict_dataset<-function(model,ms_setup,ddiag,dexpt){
@@ -56,7 +57,8 @@ predict_dataset<-function(model,ms_setup,ddiag,dexpt){
   if(dim(fm)[1]==0) return(data.frame())
   fmapped<-mapMZ(m,fm)
 }
-load_dataset<-function(model,ms_setup,ddiag,dexpt){
+
+load_dataset<-function(ms_setup,ddiag,dexpt){
   fmfname<-get_fm_fname(ms_setup,ddiag,dexpt)
   fpath<-paste0(path,fmfname)
   if(file.exists(fpath)){
@@ -64,4 +66,24 @@ load_dataset<-function(model,ms_setup,ddiag,dexpt){
   }else{
     return(data_frame())
   }
+}
+
+get_pat_df<-function(fm){
+  if(dim(fm)[2]>2){
+  return(unique(fm[,c("patientid","diagnosis")]))
+  }else{
+    return(data.frame())
+  }
+}
+
+get_spec_df<-function(fm){
+  if(dim(fm)[2]>2){
+    return(unique(fm[,c("spectrumid","patientid","diagnosis")]))
+  }else{
+    return(data.frame())
+  }
+}
+
+get_dim<-function(fm){
+  return(data.frame(nrow=dim(fm)[1],ncol=dim(fm)[2]))
 }
--- R/plan.R
+++ R/plan.R
@@ -1,28 +1,49 @@
 plan <- drake_plan(
   ms_setup=target(c(res=r,mode=m,mz=z),
-                  transform = cross(r=!!c(1,2),
+                  transform = cross(r=!!c(2),
                                     m=!!c(1,2),
                                     z=!!c(1,2))),
-  model = target(
-    get_model(ms_setup,diag,expt,method,idx),
+  fm = target(
+    load_dataset(ms_setup,diag,expt),
     # Define an analysis target for each combination of
     # tuning_setting, mean_value, and model_function.
     transform = cross(
-      ms_setup,diag=!!c(3,6,15),expt=!!c(1,2),
-      method = c("RF", "XGB"),
-      idx = !!c("pat","spec"),
-      .id = c(diag,expt,ms_setup,method,idx),.tag_out=dataset
+      ms_setup,diag=!!c(3,6),expt=!!c(1,2),
+      .id = c(diag,expt,ms_setup),.tag_out=dataset
     )
   ),
-  predict=target(
-    predict_dataset(model,ms_setup,ddiag,dexpt),
-    transform = cross(
-      model,ddiag=!!c(3,6,15),dexpt=!!c(1,2),
-      .id = c(diag,expt,ms_setup,method,idx,ddiag,dexpt),.tag_out=preds
-    )
-  ),
-  matrix=target(
-    bind_rows(preds,.id=preds),
-    transform = combine(preds,.by=c(method,idx,ms_setup))
-  )
+  patDF = target(get_pat_df(fm),
+                 transform = map(fm)),
+  specDF = target(get_spec_df(fm),
+                  transform = map(fm)),
+  dimPat=target(get_dim(patDF),transform = map(patDF)),
+  dimSpec=target(get_dim(specDF),transform = map(specDF)),
+  patStat= target(bind_rows(dimPat),transform = combine(
+    dimPat)),
+  specStat= target(bind_rows(dimSpec),transform = combine(
+    dimSpec)),
+  wrtPatStat=write.csv(patStat,file='patStat.csv'),
+  wrtSpecStat=write.csv(specStat,file='specStat.csv')
+  # model = target(
+  #   get_model(ms_setup,diag,expt,method,idx),
+  #   # Define an analysis target for each combination of
+  #   # tuning_setting, mean_value, and model_function.
+  #   transform = cross(
+  #     ms_setup,diag=!!c(3,6,15),expt=!!c(1,2),
+  #     method = c("RF", "XGB"),
+  #     idx = !!c("pat","spec"),
+  #     .id = c(diag,expt,ms_setup,method,idx),.tag_out=dataset
+  #   )
+  # ),
+  # predict=target(
+  #   predict_dataset(model,ms_setup,ddiag,dexpt),
+  #   transform = cross(
+  #     model,ddiag=!!c(3,6,15),dexpt=!!c(1,2),
+  #     .id = c(diag,expt,ms_setup,method,idx,ddiag,dexpt),.tag_out=preds
+  #   )
+  # ),
+  # matrix=target(
+  #   bind_rows(preds,.id=preds),
+  #   transform = combine(preds,.by=c(method,idx,ms_setup))
+  # )
 )
