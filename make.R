eee99bdedb96a81721bd78044ddd1376d6d1c719
lptolik <lptolik@gmail.com>
2020-04-28 19:18:27 +0900

    atttempt to run drake

--- make.R
+++ make.R
@@ -1,7 +1,18 @@
 source("R/functions.R") # defines the create_plot() function
 source("R/plan.R")      # creates the drake plan
-# options(clustermq.scheduler = "multicore") # optional parallel computing. Also needs parallelism = "clustermq"
+library(drake)
+
+# Create the template file. You may have to modify it.
+drake_hpc_template_file("slurm_clustermq.tmpl")
+
+# Configure clustermq.
+options(clustermq.scheduler = "slurm", template = "slurm_clustermq.tmpl")
+
 make(
-  plan, # defined in R/plan.R
-  verbose = 2
+  plan, max_expand = 8,# defined in R/plan.R
+  verbose = 2, 
+  parallelism = "clustermq",
+  jobs = 8,
+  console_log_file = "drake.log"
+
 )
