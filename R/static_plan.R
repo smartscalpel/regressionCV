133f9ddaca6d238f98d473597ee537267616b8dd
Anatoly Sorokin <lptolik@gmail.com>
2020-08-14 10:35:18 +0100

    all filters and plots

--- R/static_plan.R
+++ R/static_plan.R
@@ -6,9 +6,9 @@ plan <- drake_plan(
     # tuning_setting, mean_value, and model_function.
     transform = cross(
       res=!!c(2),
-      mode=!!c(1),#,2),
-      mz=!!c(2),#,1),
-      diag=!!c(3),#,6),
+      mode=!!c(1,2),
+      mz=!!c(2,1),
+      diag=!!c(3,6),
       .id = c(diag,res,mode,mz),.tag_out=dataset
     )
   ),
@@ -27,7 +27,7 @@ plan <- drake_plan(
   wrtSpecStat=write.csv(specStat,file=file_out('specStat.csv')),
   smpl_splited_fm=target(smpl_split_fm(fm,split=0.75),transform = map(fm)),# split feature matrix into train/test parts by patientid
   normalized_fm=target(normalize(fm=smpl_splited_fm,normtype),transform = cross(smpl_splited_fm,normtype=!!c('None'))),#,'Autoscaling','Pareto'))),# scale feature matrix \cite{vandenBerg:2006hm}
-  filter_fm=target(feature_filter(fm=normalized_fm,ftype),transform = cross(normalized_fm,ftype=!!filtertypes)),# reduce feature space 
+  filter_fm=target(feature_filter(fm=normalized_fm,ftype),transform = cross(normalized_fm,ftype=!!c('None','ZVar','Corr'))),# reduce feature space 
   # splited_fm=target(transform = map(normalized_fm)),# split feature matrix into train/test parts by spectrumid with respect to percentage
   # pca=target(transform = cross(fm=normalized_fm,color=!!c('diagnosis','spectrumid','patientid'))),# make PCA plots for transformed feature matrices
   # umap=target(transform = cross(fm=normalized_fm,color=!!c('diagnosis','spectrumid','patientid'))),# make umap plots for transformed feature matrices
@@ -35,10 +35,14 @@ plan <- drake_plan(
   test_rf=target(test_model(rf_cv10),transform = map(rf_cv10)),
   plot_rf_point=target(plot_test_point(fm=test_rf),transform = map(test_rf)),
   plot_rf_box=target(plot_test_box(fm=test_rf),transform = map(test_rf)),
+  plot_rf_pointT=target(plot_train_point(fm=test_rf),transform = map(test_rf)),
+  plot_rf_boxT=target(plot_train_box(fm=test_rf),transform = map(test_rf)),
   xgb_cv10=target(train_model(fm=filter_fm,modeltype='xgb'),transform = map(filter_fm)),# train regression model with CV10
   test_xgb=target(test_model(xgb_cv10),transform = map(xgb_cv10)),
   plot_xgb_point=target(plot_test_point(fm=test_xgb),transform = map(test_xgb)),
   plot_xgb_box=target(plot_test_box(fm=test_xgb),transform = map(test_xgb)),
+  plot_xgb_pointT=target(plot_train_point(fm=test_xgb),transform = map(test_xgb)),
+  plot_xgb_boxT=target(plot_train_box(fm=test_xgb),transform = map(test_xgb)),
   save_plot_xgb_point = target(ggsave(
     filename = file_out(sprintf('peak2019.diag_%d.res_%d.mode_%d.mz_%d.ftype_%s.norm_%s.cv10.%s.plot_point.pdf',
                                 diag,res,mode,mz,normtype,ftype,"xgb")),
@@ -46,6 +50,13 @@ plan <- drake_plan(
     width = 8,
     height = 8
   ),transform = map(plot_xgb_point)),
+  save_plot_xgb_pointT = target(ggsave(
+    filename = file_out(sprintf('peak2019.diag_%d.res_%d.mode_%d.mz_%d.ftype_%s.norm_%s.cv10.%s.plot_point.train.pdf',
+                                diag,res,mode,mz,normtype,ftype,"xgb")),
+    plot = plot_xgb_pointT,
+    width = 8,
+    height = 8
+  ),transform = map(plot_xgb_pointT)),
   save_plot_rf_point = target(ggsave(
     filename = file_out(sprintf('peak2019.diag_%d.res_%d.mode_%d.mz_%d.ftype_%s.norm_%s.cv10.%s.plot_point.pdf',
                                 diag,res,mode,mz,normtype,ftype,"rf")),
@@ -53,6 +64,13 @@ plan <- drake_plan(
     width = 8,
     height = 8
   ),transform = map(plot_rf_point)),
+  save_plot_rf_pointT = target(ggsave(
+    filename = file_out(sprintf('peak2019.diag_%d.res_%d.mode_%d.mz_%d.ftype_%s.norm_%s.cv10.%s.plot_point.train.pdf',
+                                diag,res,mode,mz,normtype,ftype,"rf")),
+    plot = plot_rf_pointT,
+    width = 8,
+    height = 8
+  ),transform = map(plot_rf_pointT)),
   save_plot_xgb_box = target(ggsave(
     filename = file_out(sprintf('peak2019.diag_%d.res_%d.mode_%d.mz_%d.ftype_%s.norm_%s.cv10.%s.plot_box.pdf',
                                 diag,res,mode,mz,normtype,ftype,"xgb")),
@@ -60,13 +78,27 @@ plan <- drake_plan(
     width = 8,
     height = 8
   ),transform = map(plot_xgb_box)),
+  save_plot_xgb_boxT = target(ggsave(
+    filename = file_out(sprintf('peak2019.diag_%d.res_%d.mode_%d.mz_%d.ftype_%s.norm_%s.cv10.%s.plot_box.train.pdf',
+                                diag,res,mode,mz,normtype,ftype,"xgb")),
+    plot = plot_xgb_boxT,
+    width = 8,
+    height = 8
+  ),transform = map(plot_xgb_boxT)),
   save_plot_rf_box = target(ggsave(
     filename = file_out(sprintf('peak2019.diag_%d.res_%d.mode_%d.mz_%d.ftype_%s.norm_%s.cv10.%s.plot_box.pdf',
                                 diag,res,mode,mz,normtype,ftype,"rf")),
     plot = plot_rf_box,
     width = 8,
     height = 8
-  ),transform = map(plot_rf_box))#,
+  ),transform = map(plot_rf_box)),
+  save_plot_rf_boxT = target(ggsave(
+    filename = file_out(sprintf('peak2019.diag_%d.res_%d.mode_%d.mz_%d.ftype_%s.norm_%s.cv10.%s.plot_box.train.pdf',
+                                diag,res,mode,mz,normtype,ftype,"rf")),
+    plot = plot_rf_boxT,
+    width = 8,
+    height = 8
+  ),transform = map(plot_rf_boxT))#,
   #report = knit(knitr_in("report.Rmd"), file_out("report.md"), quiet = TRUE)
   
   # model = target(
